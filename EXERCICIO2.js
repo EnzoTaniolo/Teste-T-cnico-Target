def040f325b143c4e48ce7b30c4dc91b5ef2e1e2
EnzoTaniolo <101295426+EnzoTaniolo@users.noreply.github.com>
2024-09-04 16:26:18 -0300

    Arrumando EXERCICIO2.js

--- EXERCICIO2.js
+++ EXERCICIO2.js
@@ -1,27 +1,24 @@
 
-function pertenceAFibonacci(numero) {
+function verificaFibonacci(numero) {
   let a = 0
   let b = 1
 
   if (numero === a || numero === b) {
-      return true
+    return `${numero} pertence à sequência de Fibonacci.`
   }
 
   while (b < numero) {
-      const temp = a
-      a = b
-      b = temp + b
+    const temp = a
+    a = b
+    b = temp + b
   }
 
-  return b === numero
+  if (b !== numero) {
+    return `${numero} não pertence à sequência de Fibonacci.`
+  }
+    
+  return `${numero} pertence à sequência de Fibonacci.`
+
 }
 
-
-const numero = 34
-const pertence = pertenceAFibonacci(numero)
-
-if (pertence) {
-  console.log(`${numero} pertence à sequência de Fibonacci.`)
-} else {
-  console.log(`${numero} não pertence à sequência de Fibonacci.`)
-}
+console.log(verificaFibonacci(1))
